7199adf53ca64241bf6f763d2d28f1f48ffcaf68
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-16 13:16:32 +0200

    chore: a new start...

--- frontend/src/components/albums/index.js
+++
@@ -1,4 +0,0 @@
-export { AlbumCard } from './AlbumCard'
-export { AlbumGrid } from './AlbumGrid'
-export { EditMetadataModal } from './EditMetadataModal'
-export { MetadataDiffModal } from './MetadataDiffModal'
