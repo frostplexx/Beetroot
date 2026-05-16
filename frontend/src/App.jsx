3485ca4f70710e5b7d471a88ab234ac4ffbf2cbc
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-16 09:36:04 +0200

    chore: make toasts centered

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
               </PreviewProvider>
             </Router>
           </ConfigErrorProvider>
-          <Toaster />
+          <Toaster position="top-center" />
         </TooltipProvider>
       </QueryClientProvider>
     </div>
