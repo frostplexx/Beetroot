f9ced6817bf40af121097cf00808e5113c18ddb7
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-21 12:14:51 +0200

    fix;: add back postinstall.js

--- scripts/check-schema.ts
+++
@@ -1,9 +0,0 @@
-import db from "../lib/music/database/db"
-
-console.log("Albums table schema:")
-const schema = db.prepare("PRAGMA table_info(albums)").all()
-console.log(JSON.stringify(schema, null, 2))
-
-console.log("\nSample album:")
-const sample = db.prepare("SELECT * FROM albums LIMIT 1").get()
-console.log(JSON.stringify(sample, null, 2))
