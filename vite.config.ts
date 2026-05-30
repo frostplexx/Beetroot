c35284ca3161c64364bddd57dc85a5dac254baa5
Daniel <62436912+frostplexx@users.noreply.github.com>
2026-05-30 12:16:21 +0200

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    base: "https://beet.int.kuipr.de",
+    base: "/Beetroot/",
     plugins: [
         tsconfigPaths(),
         tailwindcss(),
